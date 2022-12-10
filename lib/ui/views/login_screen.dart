58bd5b0f1ba0a46c72cedd9948407cef1d2be061
sadman_ichha <sadmanichha1@gmail.com>
2022-12-10 16:56:13 +0600

    forget password

--- lib/ui/views/login_screen.dart
+++ lib/ui/views/login_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:royal_property/const/app_colors.dart';
 import 'package:royal_property/const/app_images.dart';
 import 'package:royal_property/routes/route.dart';
 import 'package:royal_property/ui/styles/style.dart';
@@ -34,22 +35,37 @@ class LogInScreen extends StatelessWidget {
                     fontWeight: FontWeight.w400,
                     color: Color(0xFF7D848D))),
             SizedBox(height: 36.0.h),
-            customTextField(
-                "Name", _nameController, TextInputType.emailAddress),
-            SizedBox(height: 32.0.h),
+
             customTextField(
                 "your@gmail.com", _emailController, TextInputType.emailAddress),
             SizedBox(height: 32.0.h),
             passwordField(_passwordController, TextInputType.none),
+            SizedBox(height: 16.0.h),
+            Padding(
+              padding: EdgeInsets.only(left: 24.0.w, right: 24.0.w),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  InkWell(
+                    onTap: () {},
+                    child: Text("Forget Password?",
+                        style: TextStyle(
+                            fontSize: 16.0.sp,
+                            fontWeight: FontWeight.w400,
+                            color: AppColors.appBlueColor)),
+                  ),
+                ],
+              ),
+            ),
             SizedBox(height: 81.0.h),
-            BlueButton("Sign Up", () {}),
+            BlueButton("Sign In", () {}),
             SizedBox(height: 40.0.h),
-            AppStyles().richText("Have an account?", " Sign In", login),
+            AppStyles().richText("Don’t have an account?", " Sign Up", login),
             SizedBox(height: 16.0.h),
             Text("Or connect", style: AppStyles.textStyle),
             SizedBox(height: 46.0.h),
             // SizedBox(height: 25.0.h),
-           Row(
+            Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 InkWell(
@@ -80,7 +96,6 @@ class LogInScreen extends StatelessWidget {
                 ),
               ],
             )
-         
           ],
         ),
       ),
