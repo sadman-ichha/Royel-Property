20f98dde418543020d204d6de85c770fdad88897
sadman_ichha <sadmanichha1@gmail.com>
2022-12-10 17:19:17 +0600

    forget screen

--- lib/ui/views/forget_screen.dart
+++ lib/ui/views/forget_screen.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:royal_property/ui/styles/style.dart';
+import 'package:royal_property/ui/widgets/custom_text_field.dart';
 import 'package:royal_property/ui/widgets/white_appbar.dart';
 
 class ForgetPasswordScreen extends StatelessWidget {
-  const ForgetPasswordScreen({super.key});
+  TextEditingController _emailController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -11,9 +13,9 @@ class ForgetPasswordScreen extends StatelessWidget {
       appBar: WhiteAppBar("Skip", () {}),
       body: Column(
         // mainAxisAlignment: MainAxisAlignment.center,
-       // crossAxisAlignment: CrossAxisAlignment.center,
+        // crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-           SizedBox(height: 37.0.h),
+          SizedBox(height: 37.0.h),
           Center(
             child: Text("Forgot password",
                 style: TextStyle(
@@ -21,6 +23,15 @@ class ForgetPasswordScreen extends StatelessWidget {
                     fontWeight: FontWeight.w700,
                     color: const Color(0xFF1B1E28))),
           ),
+          SizedBox(height: 12.0.h),
+          Text(
+            "Enter your email account to reset \n your password",
+            textAlign: TextAlign.center,
+            style: AppStyles.textStyle,
+          ),
+          SizedBox(height: 37.0.h),
+          customTextField(
+              "your@gmail.com", _emailController, TextInputType.emailAddress)
         ],
       ),
     );
